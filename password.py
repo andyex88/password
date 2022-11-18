a26548190a1a7b99e0e9ec7b4e163f9eb9c4ae54
Andy Huang <andyex88@gmail.com>
2022-11-18 15:10:44 -0700

    Eng Ver

--- password.py
+++ password.py
@@ -1,13 +1,13 @@
 password = 'a123456'
 i = 3
 while i > 0:
-	name = input('請輸入密碼： ')
+	name = input('Please enter a password: ')
 	if name == password:
-		print('登入成功')
+		print('Successful access!')
 		break
 	else:
 		i = i - 1
 		if i  == 0:
 			break
-		print('密碼錯誤！ 還有', i ,'次機會')
+		print('Wrong password!', i ,'more chances left')
 		
